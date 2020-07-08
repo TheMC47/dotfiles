737d4c7341f3738f40287261e4f3ed0138bfef74
Yecine Megdiche <megdiche@in.tum.de>
2020-07-08 09:08:11 +0200

    initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+;; ((python-mode
+;;   (flycheck-checker . python-flake8)))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode
+  (flycheck-checker . python-flake8)))
---
+++ .local/share/konsole/Shell.profile
@@ -0,0 +1,7 @@
+[Appearance]
+ColorScheme=Sweet
+Font=Noto Mono,11,-1,5,50,0,0,0,0,0
+
+[General]
+Name=Shell
+Parent=FALLBACK/
